f76652e0a1e5ecf0bd9522e291980600a7ed0466
prabakaran.sangameswaran <prabakaran.sangameswaran@syncfusion.com>
2020-06-29 11:52:32 +0530

    CardView-EditMode sample committed.

---
+++ Samples/CardView-EditMode/ViewModel/ViewModel.cs
@@ -0,0 +1,47 @@
+﻿using Syncfusion.Windows.Shared;
+using Syncfusion.Windows.Tools.Controls;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+
+namespace CardView_EditMode
+{
+    public class ViewModel : NotificationObject
+    {
+        private ObservableCollection<CardViewModel> cardViewItems
+        public ICommand BeginEditCommand { get; }
+        public ICommand EndEditCommand { get; }
+        public ObservableCollection<CardViewModel> CardViewItems
+        {
+            get { return cardViewItems; }
+            set
+            {
+                cardViewItems = value;
+                this.RaisePropertyChanged("CardViewItems");
+            }
+        }
+
+        private void EndEditCommandMethod(object obj)
+        {
+            (obj as CardView).EndEdit();
+        }
+
+        private void BeginEditCommandMethod(object obj)
+        {
+            (obj as CardView).BeginEdit();
+
+        }
+        private void populateItems()
+        {
+            CardViewItems.Add(new CardViewModel() { Name = "John", Age = 23 });
+            CardViewItems.Add(new CardViewModel() { Name = "Mark", Age = 26 });
+            CardViewItems.Add(new CardViewModel() { Name = "Steven", Age = 25 });
+        }
+        public ViewModel()
+        {
+            CardViewItems = new ObservableCollection<CardViewModel>();
+            populateItems();
+            BeginEditCommand = new DelegateCommand(BeginEditCommandMethod);
+            EndEditCommand = new DelegateCommand(EndEditCommandMethod);
+        }
+    }
+}

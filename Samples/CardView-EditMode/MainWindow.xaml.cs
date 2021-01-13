a693da167bc03272ceb16fd71f3c5a604ba39c25
prabakaran-sangameswaran <57438309+prabakaran-sangameswaran@users.noreply.github.com>
2021-01-13 14:31:34 +0530

    Update MainWindow.xaml.cs

--- Samples/CardView-EditMode/MainWindow.xaml.cs
+++ Samples/CardView-EditMode/MainWindow.xaml.cs
@@ -24,6 +24,7 @@ namespace CardView_EditMode
         {
             InitializeComponent();
             cardView.GroupCards("Age");
+            cardView.GroupCards("FirstName");
         }
     }
 }
